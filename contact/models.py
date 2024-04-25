8e0fdfb8a3fb87ef8ad4b5b205d70bf09b1ce743
Eduardo Silva <eduardocgsilva1@gmail.com>
2024-04-25 12:11:45 -0300

    Model category, tem erro

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+   
+   def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
@@ -8,6 +17,13 @@ class Contact(models.Model):
     email = models.EmailField(max_length=200, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(
+        Category,
+          on_delete=models.SET_NULL,
+          blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
