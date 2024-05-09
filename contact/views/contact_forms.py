05571f59a9565773a0707ae31fe7bcf9d107c9e0
Eduardo Silva <eduardocgsilva1@gmail.com>
2024-05-09 12:18:22 -0300

    Creating update and delete

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -10,7 +10,7 @@ def create(request):
     form_action = reverse('contact:create')
     
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
         
         context = {
              'form': form,
@@ -64,3 +64,24 @@ def update(request, contact_id):
          }
     
     return render(request, 'contact/create.html', context)
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+    print('confirmation', confirmation)
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact' :contact,
+            'confirmation': confirmation,
+        }
+    )
+  
